07e97be811d7c792dacc6e4f881ab5e5eca9dab5
Ashutosh Behera <abehera920@gmail.com>
2022-10-12 21:02:39 +0000

    Update UserDetails.jsx useParam hook, to add url params

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export const UserDetails = () => {
+  const params = useParams();
+  const userId = params.userId;
   return (
     <>
-      <h3>UserDetails</h3>
+      <h3>Details about user {userId}</h3>
     </>
   );
 };
