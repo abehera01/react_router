4534d59aaba32b0779d1df659df2141850a8b5fa
Ashutosh Behera <abehera920@gmail.com>
2022-10-12 21:28:11 +0000

    Update Users.jsx useSearchParams

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,12 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+  const [searchParams, SetSearchParams] = useSearchParams();
   return (
     <>
       <h3>User 1</h3>
       <h3>User 2</h3>
       <h3>User 3</h3>
       <Outlet />
+      <div>
+        <button onClick={() => SetSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => SetSearchParams({})}>Reset Users</button>
+      </div>
     </>
   );
 };
