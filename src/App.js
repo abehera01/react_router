4c3016d12c3c96c1e50b6337cb0d14318a9b31d4
Ashutosh Behera <abehera920@gmail.com>
2022-10-12 22:13:00 +0000

    Update App.js and About.jsx Lazy Load page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Components/Home";
-import { About } from "./Components/About";
+// import { About } from "./Components/About";
 import { Navbar } from "./Components/Navbar";
 import { OrderSummary } from "./Components/OrderSummary";
 import { Products } from "./Components/Products";
@@ -11,6 +11,7 @@ import { Users } from "./Components/Users";
 import { UserDetails } from "./Components/UserDetails";
 import { Admin } from "./Components/Admin";
 
+const LazyAbout = React.lazy(() => import("./Components/About"));
 
 export default function App() {
   return (
@@ -18,16 +19,24 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route
+          path="/about"
+          element={
+            <React.Suspense fallback="Looding...">
+              <LazyAbout />
+            </React.Suspense>
+          }
+        />
         <Route path="/order-summary" element={<OrderSummary />} />
         <Route path="/products" element={<Products />}>
           <Route index element={<FeaturedProducts />} />
           <Route path="feactured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
         </Route>
-/        <Route path="/users" element={<Users/>} >
-          <Route path=":userId" element={<UserDetails/>}/>
-          <Route path="admin" element={<Admin/>}/>
+        /{" "}
+        <Route path="/users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
         </Route>
         <Route path="*" element={<h3>Page Not found</h3>} />
       </Routes>
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,30 @@
 export const About = () => {
   return (
     <>
-      <h3>About</h3>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices nulla a tortor dignissim, ac faucibus nulla mollis. Nullam viverra feugiat ex. Etiam egestas turpis sed justo elementum, non tincidunt arcu euismod. Duis facilisis turpis lectus, ultricies tristique nunc volutpat et. Nam fermentum facilisis enim, ac vulputate magna congue at. Sed interdum sapien 
+        sed dolor facilisis, ut tincidunt risus elementum. Ut gravida vel nisl non suscipit. Nulla dictum mollis est sit amet faucibus. Morbi facilisis congue sem eu elementum. Maecenas ultrices feugiat magna eget finibus. Nulla facilisi. Aenean eget neque lacus. Etiam accumsan vitae ligula porttitor consequat. Nunc vitae porttitor erat. Nullam utex venenatis, porttitor orci nec
+        laoreet est. Sed et nunc vitae mi faucibus pharetra at a urna.
+        Praesent sollicitudin luctus quam, ac ornare nulla ultrices vel. Pellentesque sollicitudin tempor mauris et pretium. Etiam a eros nibh. Aliquam convallis ut dolor eu gravida. Praesent luctus purus id risus rutrum congue. Nam arcu lectus, eleifend vitae tellus vulputate, feugiat lacinia est. Cras fermentum maximus sagittis. Nam urna felis, cursus non sem ut, elementum 
+        dapibus augue. Curabitur porta nisl eu felis consequat,
+        ut tincidunt libero convallis. Vestibulum mattis dui et felis tempus, vitae finibus libero facilisis. Ut et arcu eget dolor commodo suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum sollicitudin vitae turpis in fermentum. Donec ut erat id ex consequat tempus. Suspendisse vitae placerat enim, et auctor arcu.
+        Aliquam erat volutpat. Nam sodales, mi nec tristique tempus, nisl ante malesuada ex, quis iaculis urna turpis in erat. Vivamus eget vulputate erat, varius placerat nisl. Vestibulum malesuada efficitur neque ut vehicula. Praesent bibendum placerat ornare. Proin magna ipsum, eleifend sed fermentum at, aliquet vitae ante. Pellentesque faucibus urna nec dolor tempor
+        aliquet. Curabitur porttitor nisi in felis vehicula imperdiet.
+        Nullam erat lacus, fringilla eget tempus ac, eleifend nec dui. Maecenas eget nisl hendrerit, euismod elit commodo, egestas justo. Donec risus velit, mattis vel orci id, vulputate ultricies ligula. Proin imperdiet ante ut mauris aliquam, eu pellentesque enim placerat. Suspendisse potenti. Proin sed risus mollis, rhoncus ante a, venenatis neque. Duis sit amet augue
+        id ante scelerisque varius. Morbi scelerisque pellentesque feugiat.
+        Pellentesque lorem velit, tempor sed lacinia quis, hendrerit nec risus. Suspendisse potenti. Nunc dictum pulvinar tortor, eu vulputate lacus pharetra vel. Integer non consequat tellus. Fusce orci est, pharetra vel turpis quis, auctor tincidunt nulla. Integer et mollis erat. Nam tempor eleifend hendrerit. Nullam sit amet dui nisi. Fusce rhoncus sodales tortor, et
+        tempor sapien semper vel. Praesent a dolor eget lectus molestie gravida sit amet vitae dolor.
+        Duis dignissim, sem vel scelerisque elementum, risus nisl vestibulum nisl, et placerat ipsum velit ac sem. Nulla in blandit eros. Fusce interdum varius enim. Sed metus felis, pulvinar ac neque ac, suscipit consequat tortor. Sed dictum semper tempor. Maecenas massa est, pretium quis odio in, elementum ornare nisi. Proin scelerisque mollis tortor. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed eu pellentesque eros. Sed vitae viverra purus. Nam in venenatis risus. In hac habitasse platea dictumst.
+        In sit amet arcu et elit rhoncus egestas vitae et enim. Etiam id velit est. Aenean auctor pellentesque arcu ac commodo. Nulla facilisi. Praesent quis tincidunt ipsum. In turpis orci, ullamcorper in vehicula a, fringilla ut mi. Pellentesque cursus pretium metus, vel volutpat tellus tincidunt et. Donec maximus non diam sit amet mattis. Donec nec ipsum quam. Fusce
+        tincidunt dui sapien, in hendrerit velit dignissim a. Nullam vestibulum dui nulla, ut dictum nulla feugiat vitae. Morbi iaculis, turpis id blandit sollicitudin, dui arcu pharetra libero, ut dapibus enim sem eu lorem.
+        Fusce est velit, vestibulum vel luctus non, semper eu nunc. Maecenas molestie ante libero, at auctor quam rutrum eu. Nulla sed leo neque. Nam non augue ac velit sodales sagittis. Morbi pellentesque ligula quis purus mattis, eget pulvinar leo vestibulum. Pellentesque bibendum ultrices ante, in tristique tellus vehicula ut. Suspendisse quam turpis, ornare at auctor
+        a, placerat id urna. Sed ullamcorper tristique molestie. Etiam ornare in est nec tempor. Nam et ligula sapien. Morbi eget tempor sem, ut accumsan ex.
+        Praesent id diam ut justo lobortis vehicula. Donec varius posuere euismod. Maecenas neque eros, aliquam id commodo laoreet, tristique id magna. Phasellus dapibus vitae quam ut mollis. Proin est neque, auctor eget nisl vel, molestie imperdiet sem. Cras a hendrerit nibh, sit amet auctor massa. Fusce lorem enim, viverra id sodales vitae, euismod a est. Vivamus eget
+        mauris malesuada, semper libero ut, feugiat risus. Sed pharetra pharetra dapibus. Nam orci arcu, ullamcorper quis augue ac, facilisis scelerisque nisi.
+        Nunc vel porttitor elit. Sed eu mi turpis. Aenean ipsum odio, consectetur ac lacinia non, placerat at nisi. Aliquam condimentum varius odio id mattis. Donec malesuada odio in faucibus tempor. Sed accumsan lacus mauris, in dapibus massa dictum non. Cras pharetra dui odio, eget posuere ex laoreet ut. Aenean arcu arcu, tristique in magna ac, rutrum cursus lacus.
+        Curabitur malesuada ex sed ligula varius vehicula. Quisque at urna blandit, consectetur risus ut, rhoncus sem. Nam quis ante eget velit laoreet mattis non eget metus.
+      </div>
     </>
   );
 };
