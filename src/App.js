f395d57b6465616579bb4ac06206ab1dc8027d6a
Ashutosh Behera <abehera920@gmail.com>
2022-10-12 19:53:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/order-summary" element={<OrderSummary />} />
         <Route path="/products" element={<Products />}>
+          <Route index element={<FeaturedProducts />}/>
           <Route path='feactured' element={<FeaturedProducts />}/>
           <Route path='new' element={<NewProducts />}/>
         </Route>
