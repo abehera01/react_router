057c3cf89fc4c8ac7d9139c2dc3e882d6a548c17
Ashutosh Behera <abehera920@gmail.com>
2022-10-12 16:54:16 +0000

    add page not found

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 
+// {replace:true} use in button to exit the page
+
 export const Home = () => {
   const navigate = useNavigate();
   return (
     <>
       <h3>Home</h3>
-      <button onClick={() => navigate("order-summary")}>Order</button>
+      <button onClick={() => navigate("order-summary",)}>Order</button>
     </>
   );
 };
